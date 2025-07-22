e955786654401e61f986c08f1c6bfbb14dee652a
chaserChoi <chlwogur2010@gmail.com>
2025-07-22 18:11:29 +0900

    setting - 프로젝트 세팅

---
+++ be16-2nd-lazyTeam-cookingHansu-BE/src/main/java/lazyteam/cooking_hansu/CookingHansuApplication.java
@@ -0,0 +1,13 @@
+package lazyteam.cooking_hansu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingHansuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingHansuApplication.class, args);
+	}
+
+}
---
+++ be16-2nd-lazyTeam-cookingHansu-BE/src/test/java/lazyteam/cooking_hansu/CookingHansuApplicationTests.java
@@ -0,0 +1,13 @@
+package lazyteam.cooking_hansu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingHansuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
