320bfdfd22538d92c2c120beb2ff859c13c966ba
jominhyeong97 <you@example.com>
2025-07-23 13:15:24 +0900

    feat:강의 엔티티 폴더 추가 #3

--- src/main/java/lazyteam/lecture/domain/Lecture.java
+++ src/main/java/lazyteam/lecture/domain/Lecture.java
@@ -1,4 +1,25 @@
 package lazyteam.lecture.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.ValueGenerationType;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Builder
+
 public class Lecture {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long lectureId;
+
+    //fk설정 필요(조회용)
+    private Long submittedId;
+    private Long rejectAdminId;
 }
