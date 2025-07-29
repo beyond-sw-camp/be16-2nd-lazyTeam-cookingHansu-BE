919a004576994631d68011b8df0c99b7e785ede4
SeungJi20 <lina6709@naver.com>
2025-07-29 10:07:19 +0900

    feat: Recipe 엔티티 추가

---
+++ src/main/java/lazyteam/cooking_hansu/domain/recipe/entity/Recipe.java
@@ -0,0 +1,77 @@
+package lazyteam.cooking_hansu.domain.recipe.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "recipe")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_id", columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String thumbnailUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Level level;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(name = "cook_time", nullable = false, columnDefinition = "BIGINT UNSIGNED")
+    private int cookTime; // 조리 시간 (분)
+
+    // Enum 정의
+    public enum Level {
+        VERY_HIGH("매우 어려움"),
+        HIGH("어려움"),
+        MEDIUM("보통"),
+        LOW("쉬움"),
+        VERY_LOW("매우 쉬움");
+
+        private final String label;
+
+        Level(String label) {
+            this.label = label;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
+
+    public enum Category {
+        KOREAN("한식"),
+        CHINESE("중식"),
+        WESTERN("양식"),
+        JAPANESE("일식");
+
+        private final String label;
+
+        Category(String label) {
+            this.label = label;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
+}
