0929a98909b4c3ceb866dfd983b66fdb9b4d5d3c
astraglus03 <kkd0250@gmail.com>
2025-07-30 15:06:54 +0900

    fix: 게시글 레포지토리 주석처리 #28

--- src/main/java/lazyteam/cooking_hansu/domain/board/repository/BoardRepository.java
+++ src/main/java/lazyteam/cooking_hansu/domain/board/repository/BoardRepository.java
@@ -9,21 +9,21 @@ import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board , Long> {
 
-//    기본조회
-
-//    삭제되지 않은 게시글만 조회
-    @Query("SELECT b FROM Board b WHERE b.deletedAt IS NULL")
-    Page<Board> findAllNotDeleted(Pageable pageable);
-
-//    공개 게시글만 조회
-    @Query("SELECT b FROM Board b WHERE b.deletedAt IS NULL AND b.isOpen = true")
-    Page<Board> findAllPublic(Pageable pageable);
-
-//    특정 사용자의 게시글 조회
-    @Query
-    Page<Board> findByUserIdNotDeleted(@Param("userId") Long userId, Pageable pageable);
-
-
+////    기본조회
+//
+////    삭제되지 않은 게시글만 조회
+//    @Query("SELECT b FROM Board b WHERE b.deletedAt IS NULL")
+//    Page<Board> findAllNotDeleted(Pageable pageable);
+//
+////    공개 게시글만 조회
+//    @Query("SELECT b FROM Board b WHERE b.deletedAt IS NULL AND b.isOpen = true")
+//    Page<Board> findAllPublic(Pageable pageable);
+//
+////    특정 사용자의 게시글 조회
+//    @Query
+//    Page<Board> findByUserIdNotDeleted(@Param("userId") Long userId, Pageable pageable);
+//
+//
 
 
 }
