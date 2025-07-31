5abf3fb35d7811d4138e31707b9cb8e227e3e3c5
astraglus03 <kkd0250@gmail.com>
2025-07-31 10:13:00 +0900

    feat: 관리자 레포 생성 및 등록 서비스 추가 #30

--- src/main/java/lazyteam/cooking_hansu/domain/notice/dto/NoticeCreateDto.java
+++ src/main/java/lazyteam/cooking_hansu/domain/notice/dto/NoticeCreateDto.java
@@ -1,10 +1,12 @@
 package lazyteam.cooking_hansu.domain.notice.dto;
 
+import lazyteam.cooking_hansu.domain.admin.entity.Admin;
 import lazyteam.cooking_hansu.domain.notice.entity.Notice;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,14 +17,14 @@ public class NoticeCreateDto {
     @NotEmpty(message = "내용은 필수 입력입니다.")
     private String content;
 
-    private String imageUrl; // 이미지 URL을 추가할 수 있습니다.
+    private MultipartFile noticeImage; // 공지사항 이미지 파일
 
-    public Notice NoticeToEntity() {
+    public Notice NoticeToEntity(String imageUrl, Admin admin) {
         return Notice.builder()
                 .title(this.title)
                 .content(this.content)
-                .imageUrl(this.imageUrl)
-                .admin(null) // 관리자 정보는 나중에 설정할 수 있습니다.
+                .imageUrl(imageUrl)
+                .admin(admin)
                 .build();
     }
 }
--- src/main/java/lazyteam/cooking_hansu/domain/notice/service/NoticeService.java
+++ src/main/java/lazyteam/cooking_hansu/domain/notice/service/NoticeService.java
@@ -1,13 +1,17 @@
 package lazyteam.cooking_hansu.domain.notice.service;
 
+import lazyteam.cooking_hansu.domain.admin.entity.Admin;
+import lazyteam.cooking_hansu.domain.admin.repository.AdminRepository;
 import lazyteam.cooking_hansu.domain.notice.dto.NoticeCreateDto;
 import lazyteam.cooking_hansu.domain.notice.dto.NoticeDetailDto;
 import lazyteam.cooking_hansu.domain.notice.dto.NoticeListDto;
 import lazyteam.cooking_hansu.domain.notice.entity.Notice;
 import lazyteam.cooking_hansu.domain.notice.repository.NoticeRepository;
+import lazyteam.cooking_hansu.global.service.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.NoSuchElementException;
@@ -18,14 +22,18 @@ import java.util.NoSuchElementException;
 public class NoticeService {
 
     private final NoticeRepository noticeRepository;
+    private final AdminRepository adminRepository;
+    private final S3Uploader s3Uploader;
 
     // 공지사항 등록
     public void createNotice(NoticeCreateDto noticeCreateDto) {
         if(noticeCreateDto.getContent() ==null || noticeCreateDto.getContent().isEmpty()) {
             throw new IllegalArgumentException("제목과 내용은 필수 입력입니다.");
         }
-//       TODO: 공지사항 작성자 정보는 현재 하드코딩되어 있지만, 실제로는 인증된 사용자 정보를 가져와야함.
-        Notice notice = noticeCreateDto.NoticeToEntity();
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Admin admin = adminRepository.findByEmail(email).orElseThrow(() -> new NoSuchElementException("해당 이메일을 가진 관리자가 없습니다."));
+        String imageUrl = s3Uploader.upload(noticeCreateDto.getNoticeImage(), "notice/");
+        Notice notice = noticeCreateDto.NoticeToEntity(imageUrl, admin);
         noticeRepository.save(notice);
     }
 
