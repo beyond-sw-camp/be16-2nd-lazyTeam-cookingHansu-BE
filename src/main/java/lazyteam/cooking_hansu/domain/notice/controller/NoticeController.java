3a53cf7a70f9c65fd3d7cc0d9576ea0102b9c17b
astraglus03 <kkd0250@gmail.com>
2025-08-01 15:02:24 +0900

    fix: 공지사항 관련 controller 책임분리 #33

--- src/main/java/lazyteam/cooking_hansu/domain/notice/controller/NoticeController.java
+++ src/main/java/lazyteam/cooking_hansu/domain/notice/controller/NoticeController.java
@@ -21,16 +21,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping("/notice")
 public class NoticeController {
-
     private final NoticeService noticeService;
-
-    //     공지사항 등록
-    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/create")
-    public ResponseEntity<?> createNotice(@ModelAttribute @Valid NoticeCreateDto noticeCreateDto) {
-        noticeService.createNotice(noticeCreateDto);
-        return new ResponseEntity<>(ResponseDto.ok("공지사항이 등록되었습니다.", HttpStatus.CREATED), HttpStatus.CREATED);
-    }
 
     //     공지사항 전체 목록 조회
     @GetMapping("/list")
@@ -44,19 +35,4 @@ public class NoticeController {
         return new ResponseEntity<>(ResponseDto.ok(noticeService.findById(id), HttpStatus.OK), HttpStatus.OK);
     }
 
-    //     공지사항 수정
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/update/{id}")
-    public ResponseEntity<?> updateNotice(@ModelAttribute @Valid NoticeUpdateDto noticeUpdateDto, @PathVariable UUID id) {
-        noticeService.updateNotice(id, noticeUpdateDto);
-        return new ResponseEntity<>(ResponseDto.ok("공지사항이 수정되었습니다.", HttpStatus.OK), HttpStatus.OK);
-    }
-
-    //     공지사항 삭제
-    @DeleteMapping("/delete/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> deleteNotice(@PathVariable UUID id) {
-        noticeService.deleteNotice(id);
-        return new ResponseEntity<>(ResponseDto.ok("공지사항이 삭제되었습니다.", HttpStatus.OK), HttpStatus.OK);
-    }
 }
