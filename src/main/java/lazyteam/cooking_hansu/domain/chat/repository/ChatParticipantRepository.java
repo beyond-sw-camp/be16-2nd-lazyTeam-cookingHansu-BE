0399d363b4faae3e15398ae4add9dec59fa63e03
astraglus03 <kkd0250@gmail.com>
2025-08-03 18:33:52 +0900

    feat:채팅방 존재 유뮤 판단 및 추가 #39

--- src/main/java/lazyteam/cooking_hansu/domain/chat/repository/ChatParticipantRepository.java
+++ src/main/java/lazyteam/cooking_hansu/domain/chat/repository/ChatParticipantRepository.java
@@ -1,12 +1,15 @@
 package lazyteam.cooking_hansu.domain.chat.repository;
 
 import lazyteam.cooking_hansu.domain.chat.entity.ChatParticipant;
+import lazyteam.cooking_hansu.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, UUID> {
 
+    Optional<ChatRoom> findExsistingChatRoom(UUID userId, UUID otherUserId);
 }
--- src/main/java/lazyteam/cooking_hansu/domain/chat/service/ChatService.java
+++ src/main/java/lazyteam/cooking_hansu/domain/chat/service/ChatService.java
@@ -1,6 +1,8 @@
 package lazyteam.cooking_hansu.domain.chat.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import lazyteam.cooking_hansu.domain.chat.entity.ChatParticipant;
+import lazyteam.cooking_hansu.domain.chat.entity.ChatRoom;
 import lazyteam.cooking_hansu.domain.chat.repository.ChatMessageRepository;
 import lazyteam.cooking_hansu.domain.chat.repository.ChatParticipantRepository;
 import lazyteam.cooking_hansu.domain.chat.repository.ChatRoomRepository;
@@ -11,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -29,7 +32,29 @@ public class ChatService {
         User user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
         User otherUser = userRepository.findById(otherUserId).orElseThrow(() -> new EntityNotFoundException("상대 사용자를 찾을 수 없습니다."));
 
+        // 채팅방이 이미 존재하는지 확인
+        Optional<ChatRoom> exsistingChatRoom = chatParticipantRepository.findExsistingChatRoom(user.getId(), otherUser.getId());
+        if(exsistingChatRoom.isPresent()){
+            return exsistingChatRoom.get().getId();
+        }
 
-        return UUID.randomUUID();
+        ChatRoom newRoom = ChatRoom.builder()
+                .name(user.getName() + "과 " + otherUser.getName() + "의 채팅방")
+                .build();
+        chatRoomRepository.save(newRoom);
+
+        // 채팅방 참여자 추가
+        addParticipantToRoom(newRoom, user);
+        addParticipantToRoom(newRoom, otherUser);
+
+        return newRoom.getId();
+    }
+
+    public void addParticipantToRoom(ChatRoom chatRoom, User user) {
+        ChatParticipant chatParticipant = ChatParticipant.builder()
+                .chatRoom(chatRoom)
+                .user(user)
+                .build();
+        chatParticipantRepository.save(chatParticipant);
     }
 }
