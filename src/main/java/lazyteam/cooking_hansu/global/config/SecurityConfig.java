361ce2f1b17723d54f0e6c686d69df6cf458fc12
SeungJi20 <lina6709@naver.com>
2025-08-09 16:40:18 +0900

    feat: 알림기능 API 구현 #25

--- src/main/java/lazyteam/cooking_hansu/global/config/SecurityConfig.java
+++ src/main/java/lazyteam/cooking_hansu/global/config/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
                         "/purchase/**",
                         "/connect/**", // WebSocket 연결 엔드포인트
                         "/topic/**", // WebSocket 토픽
-                        "/publish/**" // WebSocket 메시지 발행
+                        "/publish/**", // WebSocket 메시지 발행
+                        "/api/notifications/**" // 알림
                 ).permitAll().anyRequest().authenticated())
                 .build();
     }
