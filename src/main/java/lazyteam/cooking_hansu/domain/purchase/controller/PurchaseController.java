ab18bebf34e38f2ded44ec72a17b8eb725c4f324
jominhyeong97 <you@example.com>
2025-08-26 20:03:03 +0900

    결제내역 확인 및 신고하기 버튼 추가

--- src/main/java/lazyteam/cooking_hansu/domain/purchase/controller/PurchaseController.java
+++ src/main/java/lazyteam/cooking_hansu/domain/purchase/controller/PurchaseController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -28,6 +29,7 @@ public class PurchaseController {
 
 
 
+    @PreAuthorize("hasRole('GENERAL')")
     @PostMapping("/prepay")
     public ResponseEntity<?> prepaymentSave(@RequestBody TossPrepayDto tossPrepayDto) {
         purchaseService.prepaymentSave(tossPrepayDto);
@@ -35,6 +37,7 @@ public class PurchaseController {
     }
 
 
+    @PreAuthorize("hasRole('GENERAL')")
     @PostMapping("/confirm")
     public JSONObject confirmPayment(@RequestBody @Valid TossPaymentConfirmDto tossPaymentConfirmDto) {
         System.out.println(tossPaymentConfirmDto);
