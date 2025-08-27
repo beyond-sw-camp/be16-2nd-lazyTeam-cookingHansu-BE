28e99d27dc89ce756468894d3a45c7924d25a9ba
astraglus03 <kkd0250@gmail.com>
2025-08-27 20:10:02 +0900

    fix: 알림 부분 polyfill로 수정 및 완료 #79

--- src/main/java/lazyteam/cooking_hansu/domain/notification/controller/NotificationController.java
+++ src/main/java/lazyteam/cooking_hansu/domain/notification/controller/NotificationController.java
@@ -1,10 +1,9 @@
 package lazyteam.cooking_hansu.domain.notification.controller;
 
 import lazyteam.cooking_hansu.domain.notification.dto.NotificationDto;
-import lazyteam.cooking_hansu.domain.notification.entity.Notification;
-import lazyteam.cooking_hansu.domain.notification.repository.NotificationRepository;
 import lazyteam.cooking_hansu.domain.notification.service.NotificationService;
 import lazyteam.cooking_hansu.domain.notification.sse.SseEmitterRegistry;
+import lazyteam.cooking_hansu.global.auth.dto.AuthUtils;
 import lazyteam.cooking_hansu.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,46 +20,32 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class NotificationController {
 
-    private final SseEmitterRegistry sseEmitterRegistry;
     private final NotificationService notificationService;
-    private final NotificationRepository notificationRepository;
 
     @GetMapping("/subscribe")
-    public SseEmitter subscribe(@RequestParam UUID userId) {
-        return sseEmitterRegistry.connect(userId);
+    public SseEmitter subscribe() {
+        UUID userId = AuthUtils.getCurrentUserId();
+        return notificationService.subscribeToNotifications(userId);
     }
 
     // 목록 조회
     @GetMapping
-    public ResponseEntity<?> list(@RequestParam UUID userId) {
-        List<Notification> notificationList = notificationRepository.findByRecipient_IdAndIsDeletedFalseOrderByCreatedAtDesc(userId);
-
-        List<NotificationDto> body = notificationList.stream()
-                .map(n -> NotificationDto.builder()
-                        .id(n.getId())
-                        .recipientId(n.getRecipient().getId())
-                        .content(n.getContent())
-                        .targetType(n.getTargetType())
-                        .targetId(n.getTargetId())
-                        .isRead(n.getIsRead())
-                        .createdAt(n.getCreatedAt())
-                        .build()
-                ).collect(Collectors.toList());
-
-        return ResponseEntity.ok(ResponseDto.ok(body, HttpStatus.OK));
+    public ResponseEntity<?> list() {
+        List<NotificationDto> notificationList = notificationService.getNotificationList();
+        return ResponseEntity.ok(ResponseDto.ok(notificationList, HttpStatus.OK));
     }
 
     // 읽음 처리
     @PatchMapping("/{id}/read")
-    public ResponseEntity<?> read(@PathVariable UUID id, @RequestParam UUID userId) {
-        notificationService.markRead(id, userId);
+    public ResponseEntity<?> read(@PathVariable UUID id) {
+        notificationService.markRead(id);
         return ResponseEntity.ok(ResponseDto.ok(null, HttpStatus.OK));
     }
     
     // 알림 삭제
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable UUID id, @RequestParam UUID userId) {
-        notificationService.markDeleted(id, userId);
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+        notificationService.markDeleted(id);
         return ResponseEntity.ok(ResponseDto.ok(null, HttpStatus.OK));
     }
 }
