03f15c18d1229f26cd6e9d2db155ca09ed3b1d2e
astraglus03 <kkd0250@gmail.com>
2025-09-05 19:40:15 +0900

    fix: 댓글 페이지네이션 수정 #98

--- src/main/java/lazyteam/cooking_hansu/domain/comment/controller/PostCommentController.java
+++ src/main/java/lazyteam/cooking_hansu/domain/comment/controller/PostCommentController.java
@@ -7,12 +7,13 @@ import lazyteam.cooking_hansu.domain.comment.dto.PostCommentUpdateDto;
 import lazyteam.cooking_hansu.domain.comment.service.PostCommentService;
 import lazyteam.cooking_hansu.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -29,11 +30,11 @@ public class PostCommentController {
         return new ResponseEntity<>(ResponseDto.ok(commentId, HttpStatus.CREATED), HttpStatus.CREATED);
     }
 
-//    댓글 목록 조회
+//    댓글 목록 조회 (페이지네이션)
     @GetMapping("/list/{postId}")
-    public ResponseEntity<?> getCommentList(@PathVariable UUID postId) {
-        List<PostCommentListResDto> commentList = postCommentService.findCommentList(postId);
-        return new ResponseEntity<>(ResponseDto.ok(commentList, HttpStatus.OK), HttpStatus.OK);
+    public ResponseEntity<?> getCommentList(@PathVariable UUID postId, Pageable pageable) {
+        Page<PostCommentListResDto> commentPage = postCommentService.findCommentList(postId, pageable);
+        return new ResponseEntity<>(ResponseDto.ok(commentPage, HttpStatus.OK), HttpStatus.OK);
     }
 
 //    댓글 수정
